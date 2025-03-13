b6077d376df7dcb8f9bb0fb9747f4d0faba99bca
icemoss <99278908+icemoss@users.noreply.github.com>
2025-03-13 20:21:20 +0000

    restructure arg processing

--- assemble.py
+++ assemble.py
@@ -3,7 +3,7 @@ import sys
 import json
 from pathlib import Path
 
-DEBUG = False
+DEBUG = True
 # Config file path
 CONFIG_FILE = Path("cyms_assembler_config.json")
 
@@ -56,51 +56,103 @@ file = open(filePath, "r")
 lines = file.readlines()
 file.close()
 
+unprocessedInstructions = []
 instructions = []
 fileLineNumber = 0
 lineNumber = 0
 constants = {}
 
 opcodes = {
-    "NOP": [0, 0],
-    "CPY": [1, 2],
-    "SWP": [2, 2],
-    "ADD": [3, 3],
-    "SUB": [4, 3],
-    "INC": [5, 1],
-    "DEC": [6, 1],
-    "MUL": [7, 3],
-    "DIV": [8, 3],
-    "MOD": [9, 3],
-    "AND": [10, 3],
-    "ORR": [11, 3],
-    "XOR": [12, 3],
-    "NOT": [13, 2],
-    "JMP": [14, 1],
-    "BRG": [15, 3],
-    "BNG": [16, 3],
-    "BRE": [17, 3],
-    "BNE": [18, 3],
-    "BRL": [19, 3],
-    "BNL": [20, 3],
-    "BRO": [21, 1],
-    "BNO": [22, 1],
-    "JSR": [23, 1],
-    "RTS": [24, 0],
-    "PSH": [25, 1],
-    "POP": [26, 1],
-    "RNG": [27, 1],
-    "INP": [28, 1],
-    "OUT": [29, 1],
-    "LOG": [30, 1],
-    "DRW": [31, 5],
-    "SFX": [32, 1],
-    "HLT": [33, 0]
+    "NOP": ["0", 0],
+    "CPY": ["1", 2],
+    "SWP": ["2", 2],
+    "ADD": ["3", 3],
+    "SUB": ["4", 3],
+    "INC": ["5", 1],
+    "DEC": ["6", 1],
+    "MUL": ["7", 3],
+    "DIV": ["8", 3],
+    "MOD": ["9", 3],
+    "AND": ["10", 3],
+    "ORR": ["11", 3],
+    "XOR": ["12", 3],
+    "NOT": ["13", 2],
+    "JMP": ["14", 1],
+    "BRG": ["15", 3],
+    "BNG": ["16", 3],
+    "BRE": ["17", 3],
+    "BNE": ["18", 3],
+    "BRL": ["19", 3],
+    "BNL": ["20", 3],
+    "BRO": ["21", 1],
+    "BNO": ["22", 1],
+    "JSR": ["23", 1],
+    "RTS": ["24", 0],
+    "PSH": ["25", 1],
+    "POP": ["26", 1],
+    "RNG": ["27", 3],
+    "INP": ["28", 1],
+    "OUT": ["29", 1],
+    "LOG": ["30", 0],
+    "DRW": ["31", 5],
+    "SFX": ["32", 1],
+    "HLT": ["33", 0]
 }
+
+def sanitize(string):
+    return string.replace(" ", "_").replace(":", "").replace("\"", "").replace(",", "")
+
+def parseLine(line):
+    splitLine = line.split(" ")
+    if splitLine[0] not in opcodes:
+        print(f"Error on instruction {line}: Invalid instruction {splitLine[0]}")
+        sys.exit(1)
+    instruction = [splitLine[0]]
+
+    if instruction[0] == "LOG":
+        instruction.append(sanitize(line.split(" ", 1)[1]))
+        splitLine = [splitLine[0]]
+    else:
+        instruction.append("_")
+
+    if len(splitLine) != opcodes[instruction[0]][1] + 1:
+        print(f"Error on instruction {line}: Incorrect operand count")
+        sys.exit(1)
+
+    for arg in splitLine[1:]:
+        if arg.startswith("#$$"):
+            mode = 5
+        elif arg.startswith("$$"):
+            mode = 4
+        elif arg.startswith("#$"):
+            mode = 3
+        elif arg.startswith("$"):
+            mode = 2
+        elif arg.startswith("#"):
+            mode = 1
+        else:
+            mode = 0
+        arg = arg.lstrip("#$")
+        if arg in constants:
+            arg = constants[arg]
+        try:
+            operand = str(int(arg))
+        except ValueError:
+            print(f"Error on instruction {line}: Invalid argument {arg}")
+            sys.exit(1)
+
+        instruction.append([operand, mode])
+
+    instruction += [["0", "-1"]] * (7 - len(instruction))
+    if DEBUG:
+        print(f"Instruction: {instruction}")
+    instruction[0] = opcodes[instruction[0]][0]
+    # [opcode, logText, [operand1, mode1], [operand2, mode2], [operand3, mode3], [operand4, mode4], [operand5, mode5]] 
+    return instruction
 
 for line in lines:
     fileLineNumber += 1
-    line = line.strip().split("//", 1)[0]
+    line = line.split("//", 1)[0].strip()
     if line == "":
         continue
     if line.startswith(".const"):
@@ -108,39 +160,19 @@ for line in lines:
         if len(parts) != 3:
             print(f"Error on line {fileLineNumber}: Invalid syntax {line}")
             sys.exit(1)
-        name = parts[1]
-        value = parts[2]
-        constants[name] = value
+        constants[parts[1]] = parts[2]
     elif line.startswith("@"):
         parts = line.split(" ")
         if len(parts) != 1 or len(parts[0]) == 1:
             print(f"Error on line {fileLineNumber}: Invalid syntax {line}")
             sys.exit(1)
-        name = parts[0]
-        constants[name] = str(lineNumber)
-    elif line.startswith("LOG"):
-        parts = line.split(" ", 1)
-        if len(parts) != 2:
-            print(f"Error on line {fileLineNumber}: Invalid syntax {line}")
-            sys.exit(1)
-        instruction = ["LOG"]
-        instruction += ["0"] * (5)
-        instruction.append(parts[1].replace(" ", "_").replace("(", "").replace(")", "").replace(",", "").replace(":", ""))
-        instructions.append(instruction)
-        lineNumber += 1
+        constants[parts[0]] = str(lineNumber)
     else:
-        instruction = line.split(" ")
-        if instruction[0] not in opcodes:
-            print(f"Error on line {fileLineNumber}: Invalid opcode {instruction[0]}")
-            sys.exit(1)
-        if len(instruction) != opcodes[instruction[0]][1] + 1:
-            print(f"Error on line {fileLineNumber}: Invalid number of arguments for {instruction[0]}")
-            sys.exit(1)
-        instruction += ["0"] * (6 - len(instruction))
-        instruction += ["_"]
-        instructions.append(instruction)
+        unprocessedInstructions.append(line)
         lineNumber += 1
 
+for line in unprocessedInstructions:
+    instructions.append(parseLine(line))
 
 MAX_COMMANDS_PER_TOWER = 300
 X_OFFSET = -2
@@ -149,28 +181,11 @@ schem.setBlock((X_OFFSET, 0, 0), 'command_block[facing=up]{Command:"say Killing 
 schem.setBlock((X_OFFSET, 1, -0), 'chain_command_block[facing=up]{Command:"kill @e[type=marker]",auto:1b}')
 
 if DEBUG:
-    print(instructions)
+    #print(instructions)
     print(constants)
 
 for index, instruction in enumerate(instructions):
-    opcode = opcodes[instruction[0]][0]
-    modes = [0] * 5
-
-    for i in range(1, 6):
-        modes[i - 1] += instruction[i].count('#')
-        modes[i - 1] += instruction[i].count('$') * 2
-        instruction[i] = instruction[i].replace('$', '').replace('#', '')
-        if instruction[i] in constants:
-            instruction[i] = constants[instruction[i]]
-
-        for character in instruction[i]:
-            if not (character.isdigit() or character == '-'):
-                print(f"Error: Invalid argument {character} for {instruction[0]}")
-                sys.exit(1)
-
-    if DEBUG:
-        print(index, opcode, instruction)
-    command = f'summon marker 16 {index} 0 {{Tags:[inst,persistent],CustomName:{instruction[6]},data:{{opcode:{opcode},operand1:{instruction[1]},operand2:{instruction[2]},operand3:{instruction[3]},operand4:{instruction[4]},operand5:{instruction[5]},mode1:{modes[0]},mode2:{modes[1]},mode3:{modes[2]},mode4:{modes[3]},mode5:{modes[4]}}}}}'
+    command = f'summon marker 16 {index} 0 {{Tags:[inst,persistent],CustomName:{instruction[1]},data:{{opcode:{instruction[0]},operand1:{instruction[2][0]},operand2:{instruction[3][0]},operand3:{instruction[4][0]},operand4:{instruction[5][0]},operand5:{instruction[6][0]},mode1:{instruction[2][1]},mode2:{instruction[3][1]},mode3:{instruction[4][1]},mode4:{instruction[5][1]},mode5:{instruction[6][1]}}}}}'
     schem.setBlock((X_OFFSET -index // MAX_COMMANDS_PER_TOWER, index % MAX_COMMANDS_PER_TOWER + 1, 2), f'chain_command_block[facing=up]{{Command:"{command}",auto:1b}}')
 
 for i in range(len(instructions) // MAX_COMMANDS_PER_TOWER + 1):
